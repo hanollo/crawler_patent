c46a6233884849d165fa3aaac55ab0c448b2d901
hanollo <hanollo@gmail.com>
2023-08-11 18:47:45 +0900

    특허에 대한 공통 출원인이 있을 경우, DB에 별도로 저장

--- crawler.ts
+++ crawler.ts
@@ -5,33 +5,34 @@ import sleep from 'sleep-promise';
 
 interface patent {
   company: string; // 회사명
-  corp_no : string; // 법인번호
-  title: string; // 특허명
-  application_number: string; // 출원번호
-  application_date_ymd: string; // 출원일자
-  quote: number; // 인용횟수
-  summary: string; // 특허요약
+  corp_no: string; // 법인번호
+  co_holder?: string; // 공동출원인
+  title?: string; // 특허명
+  application_number?: string; // 출원번호
+  application_date_ymd?: string; // 출원일자
+  quote?: number; // 인용횟수
+  summary?: string; // 특허요약
 }
 
-async function crawlWebsiteWithPost(corp_no: string): Promise<void> {
+async function crawlWebsiteWithPost(search_item: patent): Promise<void> {
   try {
 
     // 요청 파라미터
     const param = new URLSearchParams();
-    param.set('queryText', `RG=[${corp_no}]`);
+    param.set('queryText', `RG=[${search_item.corp_no}]`);
     param.set('searchInResultCk', 'undefined');
     param.set('next', 'MainList');
     param.set('config', 'G1111111111111111111111S111111111000000000');
     param.set('sortField', 'RANK');
     param.set('sortStage', 'DESC');
     param.set('configChange', 'Y');
-    param.set('expression', `RG=[${corp_no}]`);
-    param.set('historyQuery', `RG=[${corp_no}]`);
+    param.set('expression', `RG=[${search_item.corp_no}]`);
+    param.set('historyQuery', `RG=[${search_item.corp_no}]`);
     param.set('numPerPage', '100');
     param.set('numPageLinks', '10');
     param.set('currentPage', '1');
-    param.set('beforeExpression', `RG=[${corp_no}]`);
-    param.set('userInput', `${corp_no}`);
+    param.set('beforeExpression', `RG=[${search_item.corp_no}]`);
+    param.set('userInput', `${search_item.corp_no}`);
     param.set('searchInTrans', 'null');
     param.set('logFlag', 'Y');
     param.set('searchSaveCnt', '0');
@@ -70,7 +71,12 @@ async function crawlWebsiteWithPost(corp_no: string): Promise<void> {
         const applicationNumber = input?.attributes.value;
 
         const fonts = pat.querySelectorAll('font');
-        const company = fonts[fonts.length - 1]?.attributes.title;
+        const companies = fonts[fonts.length - 1]?.attributes.title;
+        const arr_companies = companies.split(",").map(item => item.trim());
+        const co_holder = arr_companies.filter(item => !item.includes(search_item.company));
+        const co_holder_str = co_holder.join(",");
+
+
 
         const divs = pat.querySelectorAll('div');
         const summary = divs[5]?.text?.trim();
@@ -80,9 +86,12 @@ async function crawlWebsiteWithPost(corp_no: string): Promise<void> {
 
         const date = lis[1]?.querySelector('a')?.text?.trim().slice(15,25);
 
+
+
         const row:patent = {
-          company: company ?? '', //회사명
-          corp_no : corp_no ?? '', //법인번호
+          corp_no : search_item.corp_no ?? '', //법인번호
+          company: search_item.company ?? '', //회사명
+          co_holder: co_holder_str ?? '', //공동출원인
           title: title ?? '', //특허명
           application_number: applicationNumber ?? '', //출원번호
           application_date_ymd: date ?? '', //출원일
@@ -95,9 +104,11 @@ async function crawlWebsiteWithPost(corp_no: string): Promise<void> {
     //DB : 테스트DB/backend/kipris_patent_list
     const pool = await mysqlConnect();
 
+
+
     async function insertRows() {
       await Promise.all(rows.map(async (row) => {
-        await pool.execute('INSERT INTO kipris_patent_list (company, corp_no, title, application_number, application_date_ymd, quote, summary) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE company = VALUES(company), corp_no = VALUES(corp_no), title = VALUES(title), application_date_ymd = VALUES(application_date_ymd), quote = VALUES(quote), summary = VALUES(summary)', [row.company, row.corp_no, row.title, row.application_number, row.application_date_ymd, row.quote, row.summary]);
+        await pool.execute('INSERT INTO kipris_patent_list (company, corp_no, co_holder, title, application_number, application_date_ymd, quote, summary) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE company = VALUES(company), corp_no = VALUES(corp_no), co_holder = VALUES(co_holder), title = VALUES(title), application_date_ymd = VALUES(application_date_ymd), quote = VALUES(quote), summary = VALUES(summary)', [row.company, row.corp_no, row.co_holder, row.title, row.application_number, row.application_date_ymd, row.quote, row.summary]);
       }));
     }
 
@@ -113,10 +124,21 @@ async function crawlWebsiteWithPost(corp_no: string): Promise<void> {
   }
 }
 
-//리디, 비바리퍼블리카(토스), 두나무 법인번호 목록
-const corp_nos = ['1101113870098', '1101115119080', '1101114834853'];
+const search_companies : patent[] =[
+  {
+    company: "리디 주식회사",
+    corp_no: "1101113870098",
+  },
+  {
+    company: "(주)비바리퍼블리카",
+    corp_no: "1101115119080",
+  },
+  {
+    company: "두나무 주식회사",
+    corp_no: "1101114834853",
+  }
+];
 
-corp_nos.map(corp_no => {
-  crawlWebsiteWithPost(corp_no);
+search_companies.forEach(search_company => {
+  crawlWebsiteWithPost(search_company);
 })
-
